3465353eb14ab8afccb35797f6e8175ef693ced2
Deodaro <shusilber@gmail.com>
2021-04-20 16:06:04 +1000

    Init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <!-- смысл этого класса будет понятен при знакомстве с роутами -->
+    <div class="wrapper-content">
+      <div id="app">
+        <h1>Hello</h1>
+        <img alt="Vue logo" src="./assets/logo.png">
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
