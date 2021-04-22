25af78a207c72c087a8646a0edf48b8971560d3e
Deodaro <l_r@list.ru>
2021-04-22 18:23:19 +1000

    Add Search module

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,18 @@
           <!-- передавать будем addNote -->
           <newNote :note="note" @addNote="addNote" />
 
-          <!-- title -->
           <div class="note-header">
+            <!-- title -->
             <h1>{{ title }}</h1>
+
+            <p>{{ search }}</p>
+            <!-- search -->
+            <search
+              :value="search"
+              placeholder="Find your note"
+              @search="search = $event" />
+            
+            <!-- icons controls -->
             <div class="icons">
               <svg :class="{ active: grid }" @click="grid = true" style="cursor: pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
               <svg :class="{ active: !grid }" @click="grid = false" style="cursor: pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line><line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line></svg>
@@ -25,7 +34,7 @@
           </div>
           
           <!-- note list -->
-          <notes :notes="notes" :grid="grid" @remove="removeNote" />
+          <notes :notes="notesFilter" :grid="grid" @remove="removeNote" />
 
         </div>
       </section>
@@ -39,14 +48,16 @@ import message from '@/components/Message.vue'
 // компоненты принято называть с большой буквы; при импорте - с маленькой
 import newNote from '@/components/NewNote.vue'
 import notes from '@/components/Notes.vue'
+import search from '@/components/Search.vue'
 
 export default {
   components: {
-    message, notes, newNote
+    message, notes, newNote, search
   },
   data() {
     return {
       title: 'Notes App',
+      search: '',
       message: null,
       grid: true,
       // для новой заметки
@@ -96,6 +107,26 @@ export default {
       },
       removeNote (index) {
         this.notes.splice(index, 1)
+      }
+    },
+    computed: {
+      notesFilter() {
+        // массив, который будем обходить,
+        // и строка, по которой будем фильтровать
+        let array = this.notes,
+          search = this.search
+        // если search пустой, возвращаем array
+        if (!search) return array
+        search = search.trim().toLowerCase()
+        // отфильтровать массив
+        array = array.filter(function(item) {
+          // если у заголовок item'а есть в search
+          if (item.title.toLowerCase().indexOf(search) !== -1) {
+            return item
+          }
+        })
+        // проверка на ошибку
+        return array
       }
     }
 }
